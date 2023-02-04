5a7fe422c8acd1ae7189ae51707bb4cdd3c5a9c7
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-04 13:48:17 +0700

    1234

--- demo0/lib/main.dart
+++ demo0/lib/main.dart
@@ -1,16 +1,18 @@
-
 import 'package:flutter/material.dart';
 
 main() {
   runApp(App());
 }
 
-
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Container(color: Colors.red,);
+    return MaterialApp(
+      home: Container(
+        child: Text("SWU"),
+      ),
+    );
   }
 }
