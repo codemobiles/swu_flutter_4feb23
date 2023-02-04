3d98938ab4ce3e144ca7417633e933217779c27c
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-04 14:10:48 +0700

    1234

--- demo0/lib/main.dart
+++ demo0/lib/main.dart
@@ -1,34 +1,7 @@
+import 'package:demo0/src/app.dart';
 import 'package:flutter/material.dart';
 
 main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: LoginPage(),
-    );
-  }
-}
-
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Login"),
-      ),
-    );
-  }
-}
