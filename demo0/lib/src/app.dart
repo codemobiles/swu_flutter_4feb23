3448bc1f2992b9552fa941634576e21b147b1afa
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-11 11:48:26 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -1,4 +1,5 @@
 
+import 'package:demo0/src/bloc/login/login_bloc.dart';
 import 'package:demo0/src/pages/app_routes.dart';
 import 'package:demo0/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
@@ -7,10 +8,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
+
   @override
   Widget build(BuildContext context) {
+    final loginBloc = BlocProvider<LoginBloc>(create: (context) => LoginBloc());
+
     return MultiBlocProvider(
-      providers: [],
+      providers: [loginBloc],
       child: MaterialApp(
         routes: AppRoute.all,
         home: LoginPage(),
