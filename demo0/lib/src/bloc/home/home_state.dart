f10d76fcffd95cf2ec82e04e189e8f65bf55ec73
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-11 16:05:50 +0700

    1234

--- demo0/lib/src/bloc/home/home_state.dart
+++ demo0/lib/src/bloc/home/home_state.dart
@@ -2,7 +2,7 @@ part of 'home_bloc.dart';
 
 class HomeState extends Equatable {
   final List<Youtube> youtubes;
-  HomeState({this.youtubes = []});
+  HomeState({this.youtubes = const []});
 
   HomeState copyWith({List<Youtube>? youtubes}) {
     return HomeState(youtubes: youtubes ?? this.youtubes);
