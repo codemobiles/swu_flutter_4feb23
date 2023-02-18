5bbcaff051ff5f660f496fece6bb237e737e1bf4
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-18 14:02:41 +0700

    1234

--- demo0/lib/src/bloc/login/login_bloc.dart
+++ demo0/lib/src/bloc/login/login_bloc.dart
@@ -1,35 +1,35 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:demo0/src/app.dart';
 import 'package:demo0/src/constants/network_api.dart';
 import 'package:demo0/src/models/user.dart';
+import 'package:demo0/src/pages/app_routes.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 part 'login_event.dart';
+
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   LoginBloc() : super(LoginState()) {
-
     on<LoginEventSubmit>((event, emit) async {
-
       emit(state.copyWith(status: LoginStatus.fetching));
       // delay for 1 sec.
       await Future.delayed(Duration(seconds: 1));
       final username = event.payload.username;
       final password = event.payload.password;
-      if (username == "admin" && password == "1234"){
+      if (username == "admin" && password == "1234") {
         // success
         emit(state.copyWith(status: LoginStatus.success));
         final prefs = await SharedPreferences.getInstance();
         await prefs.setString(NetworkAPI.token, "1234214124");
-      }else{
+      } else {
         // failed
         emit(state.copyWith(status: LoginStatus.failed));
       }
-
     });
 
     on<LoginEventRegister>((event, emit) {
@@ -40,7 +40,11 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
       print("Logout");
       final prefs = await SharedPreferences.getInstance();
       await prefs.clear();
-      Navigator.pop(context);
+      if (navigatorState.currentContext != null)
+        Navigator.pushReplacementNamed(
+          navigatorState.currentContext!,
+          AppRoute.login,
+        );
     });
   }
 }
