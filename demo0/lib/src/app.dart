c46ba3c404f506270352b1307eef7145c39e1b9d
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-18 12:02:00 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -2,6 +2,7 @@ import 'package:demo0/src/bloc/home/home_bloc.dart';
 import 'package:demo0/src/bloc/login/login_bloc.dart';
 import 'package:demo0/src/constants/network_api.dart';
 import 'package:demo0/src/pages/app_routes.dart';
+import 'package:demo0/src/pages/home/home_page.dart';
 import 'package:demo0/src/pages/login/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,10 +25,23 @@ class App extends StatelessWidget {
     );
   }
 
-  _loadDefaultPage() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = await prefs.getString(NetworkAPI.token);
-    print(token);
-    return LoginPage();
+  _loadDefaultPage() {
+    // final prefs = await SharedPreferences.getInstance();
+    // final token = await prefs.getString(NetworkAPI.token);
+    // print(token);
+    return FutureBuilder(
+      future: SharedPreferences.getInstance(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Container(
+            color: Colors.white,
+            alignment: Alignment.center,
+            child: Text("Loading..."),
+          );
+        }
+        final token = snapshot.data!.getString(NetworkAPI.token);
+        return token == null ? LoginPage() : HomePage();
+      },
+    );
   }
 }
