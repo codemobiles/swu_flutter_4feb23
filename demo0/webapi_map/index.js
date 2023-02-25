ee62b4af8e64ecf798be17bb5c32b3a06dc4e281
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-25 14:52:06 +0700

    1234

--- demo0/webapi_map/index.js
+++ demo0/webapi_map/index.js
@@ -1,1 +1,8 @@
-console.log("Hey")
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.json({result: "1234"})
+})
+
+app.listen(3000, ()=> console.log("server is ready at port http://localhost:3000"))
