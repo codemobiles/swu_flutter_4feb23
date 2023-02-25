5ddfa1c1c8cbf7c0af7a635255311a908c5f9ea7
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-25 13:24:42 +0700

    1234

--- demo0/lib/src/pages/map/map_page.dart
+++ demo0/lib/src/pages/map/map_page.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
 
+import 'package:demo0/src/app.dart';
 import 'package:demo0/src/constants/asset.dart';
 import 'package:demo0/src/services/common.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -42,22 +46,56 @@ class _MapPageState extends State<MapPage> {
   initState() {
     // TODO: implement initState
     super.initState();
-    _loadMarker();
+    _buildSingleMarker(position: LatLng(13.7462463, 100.5325515));
   }
 
-  Future<void> _loadMarker() async {
-    // make map marker
+  String formatPosition(LatLng pos) {
+    final lat = formatCurrency.format(pos.latitude);
+    final lng = formatCurrency.format(pos.longitude);
+    return ": $lat, $lng";
+  }
+
+  void _launchMaps({required double lat, required double lng}) async {
+    final parameter = '?z=16&q=$lat,$lng';
+
+    if (Platform.isIOS) {
+      final googleMap = Uri.parse('comgooglemaps://' + parameter);
+      final appleMap = Uri.parse('https://maps.apple.com/' + parameter);
+      if (await canLaunchUrl(googleMap)) {
+        await launchUrl(googleMap);
+        return;
+      }
+      if (await canLaunchUrl(appleMap)) {
+        await launchUrl(appleMap);
+        return;
+      }
+    } else {
+      final googleMapURL = Uri.parse('https://maps.google.com/' + parameter);
+      if (await canLaunchUrl(googleMapURL)) {
+        await launchUrl(googleMapURL);
+        return;
+      }
+    }
+    throw 'Could not launch url';
+  }
+
+  Future<void> _buildSingleMarker({required LatLng position}) async {
     final Uint8List markerIcon = await getBytesFromAsset(
-      Asset.pinSwuImage,
+      Asset.pinBikerImage,
       width: 150,
     );
 
     final BitmapDescriptor bitmap = BitmapDescriptor.fromBytes(markerIcon);
 
     final marker = Marker(
+      markerId: MarkerId(jsonEncode(position)),
       icon: bitmap,
-      markerId: MarkerId("1234"),
-      position: _initMap.target,
+      infoWindow: InfoWindow(
+        title: formatPosition(position),
+        snippet: "",
+        onTap: () => _launchMaps(lat: position.latitude, lng: position.longitude),
+      ),
+      position: position,
     );
     _markers.add(marker);
     setState(() {});
