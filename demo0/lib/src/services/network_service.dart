465176f4f1c458af831428155718d1ba0e3d9964
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-25 15:55:59 +0700

    1234

--- demo0/lib/src/services/network_service.dart
+++ demo0/lib/src/services/network_service.dart
@@ -1,3 +1,7 @@
+import 'package:dio/dio.dart';
+
+import '../constants/network_api.dart';
+
 class NetworkService {
   int count = 0;
 
@@ -5,6 +9,30 @@ class NetworkService {
   NetworkService._internal(); // SomeService();
   static final NetworkService _instance = NetworkService._internal();
   factory NetworkService() => _instance;
+
+  static final Dio _dio = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.baseUrl = NetworkAPI.baseURL;
+          return handler.next(options);
+        },
+        onResponse: (response, handler) async {
+          // await Future.delayed(Duration(seconds: 10));
+          return handler.next(response);
+        },
+        onError: (DioError e, handler) {
+          switch (e.response?.statusCode) {
+            case 301:
+              break;
+            case 401:
+              break;
+            default:
+          }
+          return handler.next(e);
+        },
+      ),
+    );
 
   method1() {
     count++;
