e8a179d69c5e8284349ab6edba62d7d2b60db56b
Lek CodeMobiles <chaiyasit.t@gmail.com>
2023-02-25 15:05:21 +0700

    1234

--- demo0/lib/src/app.dart
+++ demo0/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:demo0/src/bloc/home/home_bloc.dart';
 import 'package:demo0/src/bloc/login/login_bloc.dart';
+import 'package:demo0/src/bloc/map/map_bloc.dart';
 import 'package:demo0/src/constants/network_api.dart';
 import 'package:demo0/src/pages/app_routes.dart';
 import 'package:demo0/src/pages/home/home_page.dart';
@@ -13,7 +14,6 @@ final formatCurrency = NumberFormat('#,###.000');
 final formatNumber = NumberFormat('#,###');
 final navigatorState = GlobalKey<NavigatorState>();
 
-
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
@@ -21,9 +21,14 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     final loginBloc = BlocProvider<LoginBloc>(create: (context) => LoginBloc());
     final homeBloc = BlocProvider<HomeBloc>(create: (context) => HomeBloc());
+    final mapBloc = BlocProvider<MapBloc>(create: (context) => MapBloc());
 
     return MultiBlocProvider(
-      providers: [loginBloc, homeBloc],
+      providers: [
+        loginBloc,
+        homeBloc,
+        mapBloc,
+      ],
       child: MaterialApp(
         routes: AppRoute.all,
         home: _loadDefaultPage(),
